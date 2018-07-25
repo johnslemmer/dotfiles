3c49162390e80c84848f3453bba814e58ffee274
John Slemmer <john.slemmer@gmail.com>
2018-07-25 09:01:31 -0700

    add need apt installs for readline to work in python

--- apt.sh
+++ apt.sh
@@ -12,6 +12,8 @@ sudo apt install direnv
 sudo apt install screen
 sudo apt install tmux
 sudo apt install tree
+sudo apt-get install build-essential
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 
 # work
 sudo apt install autofs nfs-common nfs-kernel-server cifs-utils
